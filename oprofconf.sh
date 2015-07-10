99e291f2d0795c612f841eef4d5e6829889d81d9
Nate Hanford <nhanford@es.net>
2015-07-10 15:06:18 -0700

    take a fresh image and get it going with tools and dependencies
    More checks and installs, etc.

--- oprofconf.sh
+++ oprofconf.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
-#for debian
+read -p "To be run on a fresh Ubuntu installation\nWant to remove bloatware? " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+    sudo apt-get remove deja-dup gnome-contacts gnome-mines aisleriot gnome-sudoku gnome-mahjongg libreoffice-draw unity-lens-photos shotwell-common remmina thunderbird empathy transmission-gtk libreoffice-math libreoffice-calc libreoffice-impress libreoffice-writer rhythmbox brasero cheese unity-webapps-common
+fi
 #oprofile
 codename=$(lsb_release -c | awk  '{print $2}')
-if [! -n grep -q ddebs.ubuntu.com /etc/apt/sources.list]
-    then
-        #sudo tee /etc/apt/sources.list.d/ddebs.list << EOF
-        #deb http://ddebs.ubuntu.com/ ${codename}      main restricted universe multiverse
-        #deb http://ddebs.ubuntu.com/ ${codename}-security main restricted universe multiverse
-        #deb http://ddebs.ubuntu.com/ ${codename}-updates  main restricted universe multiverse
-        #deb http://ddebs.ubuntu.com/ ${codename}-proposed main restricted universe multiverse
-        #EOF
-        echo 'okay'
+if ! [-a /etc/apt/sources.list.d/ddebs.list]
+then
+sudo tee /etc/apt/sources.list.d/ddebs.list << EOF
+deb http://ddebs.ubuntu.com/ ${codename}      main restricted universe multiverse
+deb http://ddebs.ubuntu.com/ ${codename}-updates  main restricted universe multiverse
+deb http://ddebs.ubuntu.com/ ${codename}-proposed main restricted universe multiverse
+EOF
 fi
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ECDCAD72428D7C01
-wget http://toolkit.globus.org/ftppub/gt6/installers/repo/globus-toolkit-repo_latest_all.deb
-dpkg -i globus-toolkit-repo_latest_all.deb
+#This hasn't been already run
+if ! [-a ./globus-toolkit-repo_latest_all.deb]
+  then
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ECDCAD72428D7C01
+    wget http://toolkit.globus.org/ftppub/gt6/installers/repo/globus-toolkit-repo_latest_all.deb
+    dpkg -i globus-toolkit-repo_latest_all.deb
+fi
 sudo apt-get update
-sudo apt-get install linux-image-$(uname -r)-dbgsym oprofile iperf3 emacs netperf nuttcp flowgrind netpipe --assume-yes
+sudo apt-get install linux-image-$(uname -r)-dbgsym oprofile iperf3 emacs netperf nuttcp flowgrind systemtap --assume-yes
