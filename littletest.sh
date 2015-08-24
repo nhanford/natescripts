49ce0386803951a3b8d18508e5906f8d97e7f79c
Nathan Hanford <nhanford@es.net>
2015-08-24 14:53:43 -0700

    new test

--- littletest.sh
+++ littletest.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-#Nathan Hanford nhanford@es.net
-#Portions borrowed from everywhere.
-
-for i in 1 2 3 4 5 6 7 8 9 10
+for i in {1..10}
 do
-  iperf3 -c 
+   iperf3 -c 10.12.1.20 -O 5 -t 30 -w 250M -Z | tee -a local-40-40-pon-pc-1500B.txt
+done
