3276eeca5b2716d1ac154b94d29dde9bfb66aaee
Nathan Hanford <nhanford@ucdavis.edu>
2017-07-13 18:10:57 -0700

    debugging script

--- syncTest.sh
+++ syncTest.sh
@@ -20,40 +20,39 @@ done
 
 for i in htcp reno cubic
 do
-
     # 2 TCP unpaced, grabbing for max
-    ssh nhanford@192.168.120.190 iperf3 -VJc 192.168.100.192 -p 5200 -t45 --logfile T-T-190.json &
-    ssh nhanford@192.168.120.191 iperf3 -VJc 192.168.100.192 -p 5201 -t45 --logfile T-T-191.json 
+    ssh nhanford@192.168.120.190 iperf3 -i.1 -VJc 192.168.100.192 -p 5200 -t45 --logfile $i-T-T-190.json &
+    ssh nhanford@192.168.120.191 iperf3 -i.1 -VJc 192.168.100.192 -p 5201 -t45 --logfile $i-T-T-191.json 
     
     # 1 TCP unpaced, competing with a UDP paced to 1G, grabbing for max
-    ssh nhanford@192.168.120.190 iperf3 -VJc 192.168.100.192 -p 5200 -t45 --logfile T-U-190.json &
-    ssh nhanford@192.168.120.191 iperf3 -VJuc 192.168.100.192 -b 1Gbit -p 5201 -t45 --logfile T-U-191.json
+    ssh nhanford@192.168.120.190 iperf3 -i.1 -VJc 192.168.100.192 -p 5200 -t45 --logfile $i-T-U-190.json &
+    ssh nhanford@192.168.120.191 iperf3 -i.1 -VJuc 192.168.100.192 -b 1Gbit -p 5201 -t45 --logfile $i-T-U-191.json
 
     # 2 TCP paced to 400M
     ssh rootnh@192.168.120.190 tc qdisc add dev eth1 root fq maxrate 400Mbit
     ssh rootnh@192.168.120.191 tc qdisc add dev eth1 root fq maxrate 400Mbit
-    ssh nhanford@192.168.120.190 iperf3 -VJc 192.168.100.192 -p 5200 -t45 --logfile T400M-400M-190.json &
-    ssh nhanford@192.168.120.191 iperf3 -VJc 192.168.100.192 -p 5201 -t45 --logfile T400M-400M-191.json 
+    ssh nhanford@192.168.120.190 iperf3 -i.1 -VJc 192.168.100.192 -p 5200 -t45 --logfile $i-T400M-400M-190.json &
+    ssh nhanford@192.168.120.191 iperf3 -i.1 -VJc 192.168.100.192 -p 5201 -t45 --logfile $i-T400M-400M-191.json 
 
     # 2 TCP paced to 500M
     ssh rootnh@192.168.120.190 tc qdisc change dev eth1 root fq maxrate 500Mbit
     ssh rootnh@192.168.120.191 tc qdisc change dev eth1 root fq maxrate 500Mbit
-    ssh nhanford@192.168.120.190 iperf3 -VJc 192.168.100.192 -p 5200 -t45 --logfile T500M-500M-190.json &
-    ssh nhanford@192.168.120.191 iperf3 -VJc 192.168.100.192 -p 5201 -t45 --logfile T500M-500M-191.json 
+    ssh nhanford@192.168.120.190 iperf3 -i.1 -VJc 192.168.100.192 -p 5200 -t45 --logfile $i-T500M-500M-190.json &
+    ssh nhanford@192.168.120.191 iperf3 -i.1 -VJc 192.168.100.192 -p 5201 -t45 --logfile $i-T500M-500M-191.json 
     
     # 1 TCP paced to 800M, 1 TCP paced to 100M
     ssh rootnh@192.168.120.190 tc qdisc change dev eth1 root fq maxrate 800Mbit
     ssh rootnh@192.168.120.191 tc qdisc change dev eth1 root fq maxrate 100Mbit
-    ssh nhanford@192.168.120.190 iperf3 -VJc 192.168.100.192 -p 5200 -t45 --logfile T800M-100M-190.json &
-    ssh nhanford@192.168.120.191 iperf3 -VJc 192.168.100.192 -p 5201 -t45 --logfile T100M-800M-191.json 
+    ssh nhanford@192.168.120.190 iperf3 -i.1 -VJc 192.168.100.192 -p 5200 -t45 --logfile $i-T800M-100M-190.json &
+    ssh nhanford@192.168.120.191 iperf3 -i.1 -VJc 192.168.100.192 -p 5201 -t45 --logfile $i-T100M-800M-191.json 
     
     # 1 TCP paced to 900M, 1 TCP paced to 100M
     ssh rootnh@192.168.120.190 tc qdisc change dev eth1 root fq maxrate 900Mbit
-    ssh nhanford@192.168.120.190 iperf3 -VJc 192.168.100.192 -p 5200 -t45 --logfile T900M-100M-190.json &
-    ssh nhanford@192.168.120.191 iperf3 -VJc 192.168.100.192 -p 5201 -t45 --logfile T100M-900M-191.json
+    ssh nhanford@192.168.120.190 iperf3 -i.1 -VJc 192.168.100.192 -p 5200 -t45 --logfile $i-T900M-100M-190.json &
+    ssh nhanford@192.168.120.191 iperf3 -i.1 -VJc 192.168.100.192 -p 5201 -t45 --logfile $i-T100M-900M-191.json
 done
 
 d = `date +%F-%H-%M`
-mkdir mkdir ~/$d
-scp nhanford@192.168.120.190 *.json ~/$d
-scp nhanford@192.168.120.191 *.json ~/$d
+mkdir ~/$d
+scp nhanford@192.168.120.190:*.json ~/$d
+scp nhanford@192.168.120.191:*.json ~/$d
