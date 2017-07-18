038a950cd52e31d3ec3b86773f200aa3bb232cfe
Nathan Hanford <nhanford@ucdavis.edu>
2017-07-18 08:52:24 -0700

    shortened tests

--- mouseTest.sh
+++ mouseTest.sh
@@ -17,7 +17,7 @@ EOF
 done
 
 # 1 TCP unpaced, competing with a repeating bursty set of mouse flows
-ssh nhanford@192.168.120.190 iperf3 -i.1 -VJc 192.168.100.192 -p 5200 -t600 --logfile htcp-T-U-190.json &
+ssh nhanford@192.168.120.190 iperf3 -i.1 -VJc 192.168.100.192 -p 5200 -t300 --logfile htcp-T-U-190.json &
 for i in {1..200}
 do
     ssh nhanford@192.168.120.191 iperf3 -i.1 -uc 192.168.100.192 -b 1Gbit -p 5201 -t1
@@ -26,7 +26,7 @@ done
 
 # 1 TCP paced, competing with a repeating bursty set of mouse flows
 ssh rootnh@192.168.120.190 tc qdisc add dev eth1 root fq maxrate 1Gbit
-ssh nhanford@192.168.120.190 iperf3 -i.1 -VJc 192.168.100.192 -p 5200 -t600 --logfile htcp-T1000-U-190.json &
+ssh nhanford@192.168.120.190 iperf3 -i.1 -VJc 192.168.100.192 -p 5200 -t300 --logfile htcp-T1000-U-190.json &
 for i in {1..200}
 do
     ssh nhanford@192.168.120.191 iperf3 -i.1 -uc 192.168.100.192 -b 1Gbit -p 5201 -t1
@@ -35,7 +35,7 @@ done
 
 # 1 TCP paced, competing with a repeating bursty set of mouse flows
 ssh rootnh@192.168.120.190 tc qdisc change dev eth1 root fq maxrate 900Mbit
-ssh nhanford@192.168.120.190 iperf3 -i.1 -VJc 192.168.100.192 -p 5200 -t600 --logfile htcp-T900-U-190.json &
+ssh nhanford@192.168.120.190 iperf3 -i.1 -VJc 192.168.100.192 -p 5200 -t300 --logfile htcp-T900-U-190.json &
 for i in {1..200}
 do
     ssh nhanford@192.168.120.191 iperf3 -i.1 -uc 192.168.100.192 -b 1Gbit -p 5201 -t1
@@ -44,7 +44,7 @@ done
 
 # 1 TCP paced, competing with a repeating bursty set of mouse flows
 ssh rootnh@192.168.120.190 tc qdisc change dev eth1 root fq maxrate 500Mbit
-ssh nhanford@192.168.120.190 iperf3 -i.1 -VJc 192.168.100.192 -p 5200 -t600 --logfile htcp-T500-U-190.json &
+ssh nhanford@192.168.120.190 iperf3 -i.1 -VJc 192.168.100.192 -p 5200 -t300 --logfile htcp-T500-U-190.json &
 for i in {1..200}
 do
     ssh nhanford@192.168.120.191 iperf3 -i.1 -uc 192.168.100.192 -b 1Gbit -p 5201 -t1
