cc6acdbc04f40928d6c73d37b7de6de4dd9f6aa4
Nathan Hanford <nhanford@ucdavis.edu>
2017-07-23 21:45:17 -0700

    New script to ramp up rates for 2 competing tcp flows no udp yet

---
+++ ramptest.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ssh nhanford@192.168.120.192 << EOF
+nuttcp -SP5000
+nuttcp -SP5001
+EOF
+
+#kill old pacing stuff
+for i in 192.168.120.190 192.168.120.191
+do
+    ssh rootnh@$i << EOF
+ifconfig eth1 mtu 9000
+tc qdisc del dev eth1 root
+tc qdisc add dev eth1 root fq
+EOF
+done
+
+for i in {1..10}
+do
+    for j in 192.168.120.190 192.168.120.191
+    do
+        ssh rootnh@$j tc qdisc change dev eth1 root fq maxrate ${i}00Mbit
+    done
+    #Sleep processes nuttcp
+    ssh nhanford@192.168.120.190 nuttcp  -vv -T45 -i.1 -fparse 192.168.120.192 > T$i-T$i-190.txt &
+    ssh nhanford@192.168.120.191 nuttcp  -vv -T45 -i.1 -fparse 192.168.120.192 > T$i-T$i-191.txt
+    #Final wait
+done
+
+d=$(date +%F-%H-%M)
+mkdir ~/$d
+scp nhanford@192.168.120.190:~/*.txt ~/$d
+scp nhanford@192.168.120.191:~/*.txt ~/$d
+
+ssh nhanford@192.168.120.190 rm *.txt
+ssh nhanford@192.168.120.191 rm *.txt
+
+for i in 192.168.120.190 192.168.120.191
+do
+    ssh rootnh@$i tc qdisc del dev eth1 root
+done
