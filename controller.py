21dbe77df71a8d2b167fc700049734eb3246e43e
Nathan Hanford <nhanford@cs-it-6938296.dhcp.lbnl.us>
2017-08-15 15:52:34 -0700

    installs script and boilerplate for controller

--- controller.py
+++ controller.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 
+#set pacing rate for a given host
+
+
+#kick off test and watch rate
+
+
 if __name__ == '__main__':
 	main()
--- installs.sh
+++ installs.sh
@@ -4,3 +4,4 @@ for i in 190 191 192 193 194 195
 do
 	ssh rootnh@192.168.120.${i} apt install python-pip rabbitmq-server
 done
+
