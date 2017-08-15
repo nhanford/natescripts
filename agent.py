fdfb1743dae9ac8570fcfea1ed4134ecfc95c91a
Nathan Hanford <nhanford@cs-it-6938296.dhcp.lbnl.us>
2017-08-15 15:52:16 -0700

    installs script and boilerplate for controller

---
+++ agent.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import pika
+
+#Set pacing rate as received from controller
+
+
+if __name__ == '__main__':
+	main()
---
+++ installs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in 190 191 192 193 194 195
+do
+	ssh rootnh@192.168.120.${i} apt install python-pip rabbitmq-server
+done
