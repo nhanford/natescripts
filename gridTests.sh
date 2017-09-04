ab72b28ec56848c28b311facbd79d912c604878d
Nathan Hanford <nhanford@es.net>
2017-09-03 23:00:49 -0700

    gridftp script

--- gridTests.sh
+++ gridTests.sh
@@ -23,18 +23,18 @@ chmod +r /media/gridTmp/rand.img
 pkill gridftp
 pkill iperf3
 pkill nuttcp
-globus-gridftp-server -p 8$i -aa -anonymous-user 'nhanford'
+globus-gridftp-server -p 8$i -aa -anonymous-user 'nhanford'&
 EOF
 done
 
 ssh nhanford@192.168.120.192 << EOF 
 mkdir -p /media/gridTmp
 mount -t tmpfs -o size=4G tmpfs /media/gridTmp
-globus-url-copy -v 192.168.120.190:2811/media/gridTmp/rand.img /media/gridtmp/rand190.img
-globus-url-copy -v 192.168.120.191:2811/media/gridTmp/rand.img /media/gridtmp/rand191.img &
-globus-url-copy -v 192.168.120.194:2811/media/gridTmp/rand.img /media/gridtmp/rand194.img &
-globus-url-copy -v 192.168.120.195:2811/media/gridTmp/rand.img /media/gridtmp/rand195.img &
-globus-url-copy -v 192.168.120.196:2811/media/gridTmp/rand.img /media/gridtmp/rand196.img
+globus-url-copy -v 192.168.120.190:8190/media/gridTmp/rand.img /media/gridtmp/rand190.img &
+globus-url-copy -v 192.168.120.191:8191/media/gridTmp/rand.img /media/gridtmp/rand191.img &
+globus-url-copy -v 192.168.120.194:8194/media/gridTmp/rand.img /media/gridtmp/rand194.img &
+globus-url-copy -v 192.168.120.195:8195/media/gridTmp/rand.img /media/gridtmp/rand195.img &
+globus-url-copy -v 192.168.120.196:8196/media/gridTmp/rand.img /media/gridtmp/rand196.img
 EOF
 for j in 190 191 194 195 196
 do
@@ -44,11 +44,11 @@ done
 ssh nhanford@192.168.120.192 << EOF 
 mkdir -p /media/gridTmp
 mount -t tmpfs -o size=4G tmpfs /media/gridTmp
-globus-url-copy -v 192.168.120.190:2811/media/gridTmp/rand.img /media/gridtmp/rand190.img &
-globus-url-copy -v 192.168.120.191:2811/media/gridTmp/rand.img /media/gridtmp/rand191.img &
-globus-url-copy -v 192.168.120.194:2811/media/gridTmp/rand.img /media/gridtmp/rand194.img &
-globus-url-copy -v 192.168.120.195:2811/media/gridTmp/rand.img /media/gridtmp/rand195.img &
-globus-url-copy -v 192.168.120.196:2811/media/gridTmp/rand.img /media/gridtmp/rand196.img 
+globus-url-copy -v 192.168.120.190:8190/media/gridTmp/rand.img /media/gridtmp/rand190.img &
+globus-url-copy -v 192.168.120.191:8191/media/gridTmp/rand.img /media/gridtmp/rand191.img &
+globus-url-copy -v 192.168.120.194:8194/media/gridTmp/rand.img /media/gridtmp/rand194.img &
+globus-url-copy -v 192.168.120.195:8195/media/gridTmp/rand.img /media/gridtmp/rand195.img &
+globus-url-copy -v 192.168.120.196:8196/media/gridTmp/rand.img /media/gridtmp/rand196.img 
 EOF
 for i in 190 191 194 195 196
 do
