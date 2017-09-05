d21137911c55b1982fe6ae29d31e61ef3c6f1a61
Nathan Hanford <nhanford@es.net>
2017-09-05 11:23:26 -0700

    changing over to /tmp for permissions

--- gridTests.sh
+++ gridTests.sh
@@ -7,10 +7,8 @@ do
 ifconfig eth1 mtu 9000
 tc qdisc add dev eth1 root fq
 tc qdisc show dev eth1
-mkdir -p /media/gridTmp
-mount -t tmpfs -o size=1G tmpfs /media/gridTmp
-openssl rand -out /media/gridTmp/rand$i.img -base64 $(( 750 * 406322000000/550229 ))
-chmod +r /media/gridTmp/rand$i.img
+openssl rand -out /tmp/rand$i.img -base64 $(( 750 * 406322000000/550229 ))
+chmod +r /tmp/rand$i.img
 pkill gridftp
 pkill iperf3
 pkill nuttcp
@@ -19,13 +17,11 @@ EOF
 done
 
 ssh nhanford@192.168.120.192 << EOF 
-mkdir -p /media/gridTmp
-mount -t tmpfs -o size=4G tmpfs /media/gridTmp
-globus-url-copy -v ftp://192.168.120.190:8190/media/gridTmp/rand190.img file:///media/gridTmp/ > 190Tunpaced &
-globus-url-copy -v ftp://192.168.120.191:8191/media/gridTmp/rand191.img file:///media/gridTmp/ > 191Tunpaced &
-globus-url-copy -v ftp://192.168.120.194:8194/media/gridTmp/rand194.img file:///media/gridTmp/ > 194Tunpaced &
-globus-url-copy -v ftp://192.168.120.195:8195/media/gridTmp/rand195.img file:///media/gridTmp/ > 195Tunpaced &
-globus-url-copy -v ftp://192.168.120.196:8196/media/gridTmp/rand196.img file:///media/gridTmp/ > 196Tunpaced
+globus-url-copy -v ftp://192.168.120.190:8190/tmp/rand190.img file:///media/gridTmp/ > 190Tunpaced &
+globus-url-copy -v ftp://192.168.120.191:8191/tmp/rand191.img file:///media/gridTmp/ > 191Tunpaced &
+globus-url-copy -v ftp://192.168.120.194:8194/tmp/rand194.img file:///media/gridTmp/ > 194Tunpaced &
+globus-url-copy -v ftp://192.168.120.195:8195/tmp/rand195.img file:///media/gridTmp/ > 195Tunpaced &
+globus-url-copy -v ftp://192.168.120.196:8196/tmp/rand196.img file:///media/gridTmp/ > 196Tunpaced
 EOF
 for j in 190 191 194 195 196
 do
@@ -35,11 +31,11 @@ done
 ssh nhanford@192.168.120.192 << EOF 
 mkdir -p /media/gridTmp
 mount -t tmpfs -o size=4G tmpfs /media/gridTmp
-globus-url-copy -v ftp://192.168.120.190:8190/media/gridTmp/rand190.img file:///media/gridTmp/ > 190Tpaced &
-globus-url-copy -v ftp://192.168.120.191:8191/media/gridTmp/rand191.img file:///media/gridTmp/ > 191Tpaced &
-globus-url-copy -v ftp://192.168.120.194:8194/media/gridTmp/rand194.img file:///media/gridTmp/ > 194Tpaced &
-globus-url-copy -v ftp://192.168.120.195:8195/media/gridTmp/rand195.img file:///media/gridTmp/ > 195Tpaced &
-globus-url-copy -v ftp://192.168.120.196:8196/media/gridTmp/rand196.img file:///media/gridTmp/ > 196Tpaced 
+globus-url-copy -v ftp://192.168.120.190:8190/tmp/rand190.img file:///tmp/ > 190Tpaced &
+globus-url-copy -v ftp://192.168.120.191:8191/tmp/rand191.img file:///tmp/ > 191Tpaced &
+globus-url-copy -v ftp://192.168.120.194:8194/tmp/rand194.img file:///tmp/ > 194Tpaced &
+globus-url-copy -v ftp://192.168.120.195:8195/tmp/rand195.img file:///tmp/ > 195Tpaced &
+globus-url-copy -v ftp://192.168.120.196:8196/tmp/rand196.img file:///tmp/ > 196Tpaced 
 EOF
 for i in 190 191 194 195 196
 do
