6b13f5d0540faf079820e8068e395dcd46ae7be3
Nathan Hanford <nhanford@es.net>
2017-09-06 20:39:26 -0700

    cc

--- gridTests.sh
+++ gridTests.sh
@@ -19,12 +19,13 @@ globus-gridftp-server -S -p 8$i -aa -anonymous-user 'nhanford' -home-dir /
 EOF
 done
 
+# flip these
 ssh nhanford@192.168.120.192 << EOF 
-{ time globus-url-copy -vb ftp://192.168.120.190:8190/tmp/zero190.img file:///dev/null ;} 2> 190Tunpaced.txt &
-{ time globus-url-copy -vb ftp://192.168.120.191:8191/tmp/zero191.img file:///dev/null ;} 2> 191Tunpaced.txt &
-{ time globus-url-copy -vb ftp://192.168.120.194:8194/tmp/zero194.img file:///dev/null ;} 2> 194Tunpaced.txt &
-{ time globus-url-copy -vb ftp://192.168.120.195:8195/tmp/zero195.img file:///dev/null ;} 2> 195Tunpaced.txt &
-{ time globus-url-copy -vb ftp://192.168.120.196:8196/tmp/zero196.img file:///dev/null ;} 2> 196Tunpaced.txt
+time globus-url-copy -cc5 -vb ftp://192.168.120.190:8190/tmp/zero190.img file:///dev/null | tee -a 190Tunpaced.txt &
+time globus-url-copy -cc5 -vb ftp://192.168.120.191:8191/tmp/zero191.img file:///dev/null | tee -a 191Tunpaced.txt &
+time globus-url-copy -cc5 -vb ftp://192.168.120.194:8194/tmp/zero194.img file:///dev/null | tee -a 194Tunpaced.txt &
+time globus-url-copy -cc5 -vb ftp://192.168.120.195:8195/tmp/zero195.img file:///dev/null | tee -a 195Tunpaced.txt &
+time globus-url-copy -cc5 -vb ftp://192.168.120.196:8196/tmp/zero196.img file:///dev/null | tee -a 196Tunpaced.txt
 EOF
 for j in 190 191 194 195 196
 do
@@ -36,11 +37,11 @@ done
 #Sleep processes gridftp
 ssh nhanford@192.168.120.192 << EOF 
 mkdir -p /tmp
-{ time globus-url-copy -vb ftp://192.168.120.190:8190/tmp/zero190.img file:///dev/null ;} 2> 190Tpaced.txt &
-{ time globus-url-copy -vb ftp://192.168.120.191:8191/tmp/zero191.img file:///dev/null ;} 2> 191Tpaced.txt &
-{ time globus-url-copy -vb ftp://192.168.120.194:8194/tmp/zero194.img file:///dev/null ;} 2> 194Tpaced.txt &
-{ time globus-url-copy -vb ftp://192.168.120.195:8195/tmp/zero195.img file:///dev/null ;} 2> 195Tpaced.txt &
-{ time globus-url-copy -vb ftp://192.168.120.196:8196/tmp/zero196.img file:///dev/null ;} 2> 196Tpaced.txt 
+time globus-url-copy -cc5 -vb ftp://192.168.120.190:8190/tmp/zero190.img file:///dev/null | tee -a 190Tpaced.txt &
+time globus-url-copy -cc5 -vb ftp://192.168.120.191:8191/tmp/zero191.img file:///dev/null | tee -a 191Tpaced.txt &
+time globus-url-copy -cc5 -vb ftp://192.168.120.194:8194/tmp/zero194.img file:///dev/null | tee -a 194Tpaced.txt &
+time globus-url-copy -cc5 -vb ftp://192.168.120.195:8195/tmp/zero195.img file:///dev/null | tee -a 195Tpaced.txt &
+time globus-url-copy -cc5 -vb ftp://192.168.120.196:8196/tmp/zero196.img file:///dev/null | tee -a 196Tpaced.txt 
 EOF
 for i in 190 191 194 195 196
 do
