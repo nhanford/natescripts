bd0023383dab74563baba9d5b6513a0c21fcd3c2
Nathan Hanford <nhanford@es.net>
2017-09-08 21:51:52 -0700

    -p 1

--- gridTests.sh
+++ gridTests.sh
@@ -15,18 +15,19 @@ fi
 pkill gridftp
 pkill iperf3
 pkill nuttcp
+# server -z and then logpath
 globus-gridftp-server -S -p 8$i -aa -anonymous-user 'nhanford' -home-dir /
 EOF
 done
 
 ssh nhanford@192.168.120.192 << EOF 
 mkdir -p /tmp
-{ time globus-url-copy -vb ftp://192.168.100.190:8190/tmp/zero190.img file:///dev/null ;} 2> 190Tpaced.txt &
-{ time globus-url-copy -vb  ftp://192.168.100.191:8191/tmp/zero191.img file:///dev/null ;} 2> 191Tpaced.txt &
-{ time globus-url-copy -vb  ftp://192.168.100.194:8194/tmp/zero194.img file:///dev/null ;} 2> 194Tpaced.txt &
-{ time globus-url-copy -vb  ftp://192.168.100.195:8195/tmp/zero195.img file:///dev/null ;} 2> 195Tpaced.txt &
-{ time globus-url-copy -vb  ftp://192.168.100.196:8196/tmp/zero196.img file:///dev/null ;} 2> 196Tpaced.txt 
-sleep 100
+{ time globus-url-copy -p 1 ftp://192.168.100.190:8190/tmp/zero190.img file:///dev/null ;} 2> 190Tpaced.txt &
+{ time globus-url-copy -p 1 ftp://192.168.100.191:8191/tmp/zero191.img file:///dev/null ;} 2> 191Tpaced.txt &
+{ time globus-url-copy -p 1 ftp://192.168.100.194:8194/tmp/zero194.img file:///dev/null ;} 2> 194Tpaced.txt &
+{ time globus-url-copy -p 1 ftp://192.168.100.195:8195/tmp/zero195.img file:///dev/null ;} 2> 195Tpaced.txt &
+{ time globus-url-copy -p 1 ftp://192.168.100.196:8196/tmp/zero196.img file:///dev/null ;} 2> 196Tpaced.txt 
+sleep 180
 EOF
 
 for j in 190 191 194 195 196
@@ -38,12 +39,12 @@ EOF
 done
 
 ssh nhanford@192.168.120.192 << EOF 
-{ time globus-url-copy -vb ftp://192.168.100.190:8190/tmp/zero190.img file:///dev/null ;} 2> 190Tunpaced.txt &
-{ time globus-url-copy -vb ftp://192.168.100.191:8191/tmp/zero191.img file:///dev/null ;} 2> 191Tunpaced.txt &
-{ time globus-url-copy -vb ftp://192.168.100.194:8194/tmp/zero194.img file:///dev/null ;} 2> 194Tunpaced.txt &
-{ time globus-url-copy -vb ftp://192.168.100.195:8195/tmp/zero195.img file:///dev/null ;} 2> 195Tunpaced.txt &
-{ time globus-url-copy -vb ftp://192.168.100.196:8196/tmp/zero196.img file:///dev/null ;} 2> 196Tunpaced.txt
-sleep 100
+{ time globus-url-copy -p 1 ftp://192.168.100.190:8190/tmp/zero190.img file:///dev/null ;} 2> 190Tunpaced.txt &
+{ time globus-url-copy -p 1 ftp://192.168.100.191:8191/tmp/zero191.img file:///dev/null ;} 2> 191Tunpaced.txt &
+{ time globus-url-copy -p 1 ftp://192.168.100.194:8194/tmp/zero194.img file:///dev/null ;} 2> 194Tunpaced.txt &
+{ time globus-url-copy -p 1 ftp://192.168.100.195:8195/tmp/zero195.img file:///dev/null ;} 2> 195Tunpaced.txt &
+{ time globus-url-copy -p 1 ftp://192.168.100.196:8196/tmp/zero196.img file:///dev/null ;} 2> 196Tunpaced.txt
+sleep 180
 EOF
 
 d=$(date +%F-%H-%M)
@@ -54,7 +55,3 @@ mv *.txt ~/$d
 EOF
 
 scp -r nhanford@192.168.120.192:~/$d ./
-
-# Just send 0's from source
-# destination is /dev/null
-# ask Raj how to restrict to a single flow
