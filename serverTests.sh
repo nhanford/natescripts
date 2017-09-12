7039c2412c1f6a6e44ed5426d95fcb761fec14c1
Nathan Hanford <nhanford@es.net>
2017-09-12 10:29:25 -0700

    back to separate file names

--- serverTests.sh
+++ serverTests.sh
@@ -7,10 +7,10 @@ do
 ifconfig eth1 mtu 9000
 tc qdisc add dev eth1 root fq maxrate 200Mbit
 tc qdisc show dev eth1
-if [ ! -e "/tmp/zero.img" ]
+if [ ! -e "/tmp/zero$i.img" ]
 then
 	dd if=/dev/zero of=/tmp/zero.img bs=1M count=4096
-	chmod +r /tmp/zero.img
+	chmod +r /tmp/zero$i.img
 fi
 pkill gridftp
 pkill iperf3
@@ -25,7 +25,6 @@ globus-gridftp-server -S -p 8190 -aa -anonymous-user 'nhanford' -home-dir / -Z ~
 EOF
 
 globus-url-copy -cc 5 -p 1 -af alias-file -f xfer-file
-sleep 180
 
 for j in 190 191 194 195 196
 do
