64e28e7b8d7693b802334ca02c5934f6cfc0da14
Nathan Hanford <nhanford@es.net>
2017-09-12 01:49:13 -0700

    log level

--- serverTests.sh
+++ serverTests.sh
@@ -15,7 +15,7 @@ fi
 pkill gridftp
 pkill iperf3
 pkill nuttcp
-globus-gridftp-server -S -p 8$i -aa -anonymous-user 'nhanford' -home-dir / -Z ~/$i.log
+globus-gridftp-server -S -p 8$i -aa -anonymous-user 'nhanford' -home-dir / -Z ~/$i.log -log-level all
 EOF
 done
 
@@ -39,10 +39,15 @@ globus-url-copy -cc 5 -p 1 -af alias-file -f xfer-file
 sleep 180
 
 d=$(date +%F-%H-%M)
-
-ssh nhanford@192.168.120.192 << EOF
 mkdir ~/$d
-mv *.txt ~/$d
-EOF
 
-scp -r nhanford@192.168.120.192:~/$d ./
+
+for i in 190 191 194 195 196
+do
+	scp rootnh@192.168.120.$i:~/$i.log ~/$d
+	ssh rootnh@192.168.120.$i << EOF
+mkdir ~/$d
+mv *.log ~/$d
+pkill gridftp
+EOF
+done
