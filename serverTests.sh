3801040604047c62541a3dbda395eba178c89180
Nathan Hanford <nhanford@es.net>
2017-09-12 19:54:40 -0700

    quiet mode

--- serverTests.sh
+++ serverTests.sh
@@ -3,7 +3,7 @@
 # create junk files, start servers
 for i in 190 191 194 195 196
 do
-    ssh rootnh@192.168.120.$i << EOF
+    ssh -qtt rootnh@192.168.120.$i << EOF
 ifconfig eth1 mtu 9000
 tc qdisc add dev eth1 root fq maxrate 200Mbit
 tc qdisc show dev eth1
@@ -16,7 +16,7 @@ globus-gridftp-server -S -p 8$i -aa -anonymous-user 'nhanford' -home-dir / -Z ~/
 EOF
 done
 
-ssh rootnh@192.168.120.192 << EOF
+ssh -qtt rootnh@192.168.120.192 << EOF
 ifconfig eth1 mtu 9000
 globus-gridftp-server -S -p 8192 -aa -anonymous-user 'nhanford' -home-dir / -Z ~/192.log -log-level all
 EOF
@@ -26,7 +26,7 @@ sleep 10
 
 for i in 190 191 194 195 196
 do
-	ssh rootnh@192.168.120.$i << EOF 
+	ssh -qtt rootnh@192.168.120.$i << EOF 
 tc qdisc del dev eth1 root
 tc qdisc show dev eth1
 EOF
@@ -42,7 +42,7 @@ mkdir ~/$d
 for i in 190 191 192 194 195 196
 do
 	scp rootnh@192.168.120.$i:~/$i.log ~/$d
-	ssh rootnh@192.168.120.$i << EOF
+	ssh -qtt rootnh@192.168.120.$i << EOF
 mkdir ~/$d
 mv *.log ~/$d
 pkill gridftp
