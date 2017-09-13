0644edb8195097ef82f34552d95c1ed10561b0b5
Nathan Hanford <nhanford@es.net>
2017-09-12 20:51:36 -0700

    more logging

--- serverTests.sh
+++ serverTests.sh
@@ -3,7 +3,8 @@
 # create junk files, start servers
 for i in 190 191 194 195 196
 do
-    ssh -q rootnh@192.168.120.$i << EOF
+	echo "First contact to $i"
+    ssh rootnh@192.168.120.$i << EOF
 ifconfig eth1 mtu 9000
 tc qdisc add dev eth1 root fq maxrate 200Mbit
 tc qdisc show dev eth1
@@ -12,13 +13,17 @@ then
 	dd if=/dev/zero of=/tmp/zero.img bs=1M count=4096
 	chmod +r /tmp/zero.img
 fi
+ls /tmp | grep img
 globus-gridftp-server -S -p 8$i -aa -anonymous-user 'nhanford' -home-dir / -Z ~/$i.log -log-level all
+ps aux | grep gridftp
 EOF
 done
 
-ssh -q rootnh@192.168.120.192 << EOF
+echo "attempting contact to receiving server"
+ssh rootnh@192.168.120.192 << EOF
 ifconfig eth1 mtu 9000
 globus-gridftp-server -S -p 8192 -aa -anonymous-user 'nhanford' -home-dir / -Z ~/192.log -log-level all
+ps aux | grep gridftp
 EOF
 
 globus-url-copy -cc 5 -p 1 -af alias-file -f xfer-file
@@ -26,7 +31,8 @@ sleep 10
 
 for i in 190 191 194 195 196
 do
-	ssh -q rootnh@192.168.120.$i << EOF 
+	echo "second contac to $i"
+	ssh rootnh@192.168.120.$i << EOF 
 tc qdisc del dev eth1 root
 tc qdisc show dev eth1
 EOF
@@ -41,10 +47,12 @@ mkdir ~/$d
 # move logs, stop servers
 for i in 190 191 192 194 195 196
 do
+	echo "third contact to $i"
 	scp rootnh@192.168.120.$i:~/$i.log ~/$d
-	ssh -q rootnh@192.168.120.$i << EOF
+	ssh rootnh@192.168.120.$i << EOF
 mkdir ~/$d
 mv *.log ~/$d
 pkill gridftp
+ps aux | grep gridftp
 EOF
 done
