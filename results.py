7380c354634f08ed052f4c5d6d18ea5037aa23e3
Nathan Hanford <nhanford@es.net>
2017-09-15 18:55:37 -0700

    checking results checker script

--- results.py
+++ results.py
@@ -6,6 +6,6 @@ for path in glob.glob('*.log'):
 	print '\n',path
 	with open(path,'r') as fp:
 		for line in fp:
-			start = float(re.compile('START=\d+.\d+').search(line).group(0)[5:])
+			start = float(re.compile('START=\d+.\d+').search(line).group(0)[6:])
 			end = float(re.compile('DATE=\d+.\d+').search(line).group(0)[4:])
 			print end-start
